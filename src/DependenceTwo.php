72691ec29855001d1a6eab298cf616297527db9e
turncypher <turncypher@gmail.com>
2017-11-22 12:44:01 +0800

    first commit

---
+++ src/DependenceTwo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emmanuel
+ * Date: 17-11-22
+ * Time: 上午11:45
+ */
+
+namespace DependenceTwo;
+
+class DependenceTwo
+{
+    public function test()
+    {
+        echo 'dependence from github demo' . PHP_EOL;
+    }
+}
